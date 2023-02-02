c7bb670cd16c257c78e3a1256d2cae2b0c8fee97
Alex Sobiek <alex@alexsobiek.com>
2023-02-02 17:00:35 -0600

    Setup storage projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
 rootProject.name = "WorldDB"
 
 include("api")
+
+setupStorageProject("anvil")
+setupStorageProject("mongodb")
+
+fun setupStorageProject(name: String) {
+    setupSubproject(name) {
+        projectDir = file("storage/$name");
+    }
+}
+
+inline fun setupSubproject(name: String, block: ProjectDescriptor.() -> Unit) {
+    include(name)
+    project(":$name").apply(block)
+}
